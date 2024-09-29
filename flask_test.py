67fc108d92254ac3a9e2529ca0d79e259365d79f
David <wadid@umich.edu>
2024-09-28 23:22:38 -0400

    front end

--- flask_test.py
+++ flask_test.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, abort
 import os
 
 app = Flask(__name__)
@@ -12,12 +12,72 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET'])
 def index():
+    operation = request.form.get('operation')
+    heightFeet = request.form.get('heightFeet')
+    heightInch = request.form.get('heightInch')
+    weight = request.form.get('weight')
+    age = request.form.get('age')
+    gender = request.form.get('gender')
+    minutes = request.form.get('minutes')
+    days = request.form.get('days')
+
+    if(operation == 'create'):
+        return render_template('macro.html')
+    
     return render_template('submit.html')
 
 @app.route('/submit', methods=['GET'])
 def result_page():
     
     return render_template('submit.html')
+
+@app.route('/submit', methods=['POST'])
+def show_create():
+    heightFeet = (int)(request.form.get('heightInFeet'))
+    heightInch = (int)(request.form.get('heightInInches'))
+    weight = (int)(request.form.get('weight'))
+    age = (int)(request.form.get('age'))
+    gender = (int)(request.form.get('gender'))
+    plan = (int)(request.form.get('plan'))
+    days = (int)(request.form.get('days'))
+    heightFeet *= 12
+    heightInch += heightFeet
+    heightInch *= 2.54
+    calories = 0
+    if (gender == 1): 
+        bmr = (10 * weight) + (6.25 * heightInch) - (5 * age) + 5
+    else:
+        bmr = (10 * weight) + (6.25 * heightInch) - (5 * age) - 161
+    #none
+    if(days == 0):
+        calories = 1.2 * bmr
+    elif(days == 1):
+        calories = 1.375 * bmr
+    elif(days == 2):
+        calories = 1.55 * bmr
+    elif(days == 3):
+        calories = 1.725 * bmr
+    elif(days == 4):
+        calories = 1.9 * bmr
+    
+    if(plan == 0):
+        abort(400)
+    elif(plan == 1):
+        calories = calories - 500
+    elif(plan == 2):
+        calories =calories - 1000
+    elif(plan == 3):
+        calories =calories + 500
+    elif(plan == 4):
+        calories =calories + 1000
+
+    carbohydrates = (0.5 * calories) / 4
+    context = {
+        'bmr': bmr,
+        "carbohydrates": carbohydrates,
+        'calories': calories,
+    }
+    return render_template('macro.html', **context)
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
